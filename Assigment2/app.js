64c5207b6b634fe81f00e8f5ffbbe14610e35d23
jawadafzal02 <123592492+jawadafzal02@users.noreply.github.com>
2023-02-03 14:59:52 -0800

    Add files via upload

---
+++ Assigment2/app.js
@@ -0,0 +1,40 @@
+// Q1
+var a = ("Username")
+alert(a)
+
+// Q2
+var myName = ("muhammadjawad")
+
+// Q3
+var message = ("hello World")
+alert(message)
+
+// Q4
+var bio = "Muhammad jawad"
+alert (bio)
+
+bio =' 22 years old'
+alert(bio)
+
+bio ='continue web and mobile'
+alert(bio)
+
+// Q5
+var p ="pizza"
+alert(p)
+
+// Q6
+var email ="jawadafzal52923@gmail.com"
+alert(email)
+
+// Q7
+var book = 'i am learning js from this book "a smarter way to learn js"'
+alert(book)
+
+// Q8
+document.write ("yeah i can write html in javascript")
+
+
+// Q9
+var style = "----------js---------"
+alert(style)
