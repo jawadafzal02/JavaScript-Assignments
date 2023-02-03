c550035f2010cbbc05b63758fd6151bdff3531b7
jawadafzal02 <123592492+jawadafzal02@users.noreply.github.com>
2023-02-03 13:52:05 -0800

    Add files via upload

---
+++ Assigmnet1/app.js
@@ -0,0 +1,15 @@
+// Q1
+alert("HelloWorld")
+
+// Q2
+alert("Error pls enter password")
+
+// Q3
+alert("welcome to js land \n happy coding")
+
+// Q4
+alert("welcome to js land")
+alert("happy coding")
+
+// Q5
+alert("hello....! i can run js through my web browser console")
